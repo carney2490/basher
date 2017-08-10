6a7c5dbd349d5519e326981fc2fba105c9851b52
JTong <eldertong@gmail.com>
2017-08-10 11:47:50 -0400

    commenting code with john and macy

--- bash.rb
+++ bash.rb
@@ -20,30 +20,22 @@ def winner(tickets_i_boughtbers3, winning_ticket)
 end
 
 def so_close(winning_ticket, tickets_i_bought)
-    # puts "tickets_i_bought is #{tickets_i_bought}"
-    # puts "my_ticket is #{winning_ticket}"
-    position = 0
-    counter_for_reset = 0
+    position = 0 #represents each position in the string in the tickets_i_bought_array
     item_in_array = 0
     array_length = tickets_i_bought.length
     my_ticket_length = winning_ticket.length
-    # puts "array_length is #{array_length}"
     array_length.times do #this loop runs for each element in the tickets_i_bought array
         counter = 0 
         4.times do #this loop runs for each character in the string in the tickets_i_bought array
-            # puts "counter before is #{counter}"
             if winning_ticket[position] == tickets_i_bought[item_in_array][position]
                 counter = counter + 1
-                # puts "counter is #{counter}"
             end
-            # puts "position is #{position}"
             position = position + 1
             if position == 4 # if the position counter gets to 4 this will reset the position counter to 0 and add 1 to the item_in_array counter
             position = 0
             item_in_array = item_in_array + 1
             end
         end
-        # puts "counter is #{counter}"
         if counter == 4
             puts "you win"
             "you win"
